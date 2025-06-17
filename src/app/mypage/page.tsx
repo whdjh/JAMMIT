7af54a5fed80109f44d8ed9deff66c9a81670453
whdjh <99352927+whdjh@users.noreply.github.com>
2025-06-17 17:35:00 +0900

    chore: suspense 제거

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from 'react';
 import MyPageContent from '@/components/products/mypage/MyPageContent';
 
 export default async function MyPage() {
   return (
-    <Suspense fallback={'Loading...'}>
-      <MyPageContent />
-    </Suspense>
+    <MyPageContent />
   );
 }
