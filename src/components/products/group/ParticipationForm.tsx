d339914e24e32ec1f3a5fe2d03b311f1842837f2
whdjh <99352927+whdjh@users.noreply.github.com>
2025-06-18 17:50:25 +0900

    feat: [버그] textarea 변경후 formprovider 적용미흡
    Co-authored-by: whdjh <whdjh98@naver.com>

--- src/components/products/group/ParticipationForm.tsx
+++ src/components/products/group/ParticipationForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import Button from '@/components/commons/Button';
 import TextArea from '@/components/commons/Textarea';
 import TagSelector from '@/components/commons/TagSelector';
@@ -22,17 +22,19 @@ export default function ParticipationForm({
   gathering,
   onComplete,
 }: ParticipationFormProps) {
-  const {
-    handleSubmit,
-    formState: { isValid },
-    setValue,
-  } = useForm<ParticipationFormData>({
+  const methods = useForm<ParticipationFormData>({
     mode: 'onChange',
     defaultValues: {
       session: [],
       introduction: '',
     },
   });
+
+  const {
+    handleSubmit,
+    formState: { isValid },
+    setValue,
+  } = methods;
 
   const { sessions } = gathering;
 
@@ -57,51 +59,53 @@ export default function ParticipationForm({
 
   return (
     <div className="pc:w-[22.625rem] w-full">
-      <form
-        onSubmit={handleSubmit(handleFormSubmit)}
-        className="flex flex-col gap-[1.25rem]"
-      >
-        <div className="flex flex-col gap-[1.25rem] rounded-[0.5rem] bg-[#212226] p-[1.75rem]">
-          <div className="w-full text-center">
-            <ParticipationFormTitle>함께하기</ParticipationFormTitle>
-          </div>
-
-          <div>
-            <ParticipationFormTitle>신청 세션</ParticipationFormTitle>
-            <TagSelector
-              mode="selectable"
-              tags={availableTags}
-              disabledTags={disabledTags}
-              selectMode="single"
-              onChange={handleSessionChange}
-            />
-          </div>
-
-          <div>
-            <ParticipationFormTitle>간단 소개</ParticipationFormTitle>
-            <TextArea
-              name="introduction"
-              placeholder="어떤 연주를 하고 싶나요?"
-              rules={{
-                required: '소개글을 입력하세요.',
-                maxLength: {
-                  value: 500,
-                  message: '소개글은 500자 이내로 입력해주세요.',
-                },
-              }}
-            />
-          </div>
-        </div>
-
-        <Button
-          type="submit"
-          variant="solid"
-          disabled={!isValid}
-          className="pc:w-[22.75rem] w-full"
+      <FormProvider {...methods}>
+        <form
+          onSubmit={handleSubmit(handleFormSubmit)}
+          className="flex flex-col gap-[1.25rem]"
         >
-          신청 완료
-        </Button>
-      </form>
+          <div className="flex flex-col gap-[1.25rem] rounded-[0.5rem] bg-[#212226] p-[1.75rem]">
+            <div className="w-full text-center">
+              <ParticipationFormTitle>함께하기</ParticipationFormTitle>
+            </div>
+
+            <div>
+              <ParticipationFormTitle>신청 세션</ParticipationFormTitle>
+              <TagSelector
+                mode="selectable"
+                tags={availableTags}
+                disabledTags={disabledTags}
+                selectMode="single"
+                onChange={handleSessionChange}
+              />
+            </div>
+
+            <div>
+              <ParticipationFormTitle>간단 소개</ParticipationFormTitle>
+              <TextArea
+                name="introduction"
+                placeholder="어떤 연주를 하고 싶나요?"
+                rules={{
+                  required: '소개글을 입력하세요.',
+                  maxLength: {
+                    value: 500,
+                    message: '소개글은 500자 이내로 입력해주세요.',
+                  },
+                }}
+              />
+            </div>
+          </div>
+
+          <Button
+            type="submit"
+            variant="solid"
+            disabled={!isValid}
+            className="pc:w-[22.75rem] w-full"
+          >
+            신청 완료
+          </Button>
+        </form>
+      </FormProvider>
     </div>
   );
 }
