28bba13a1919c95283e70b9cc36b3139d4bb03de
whdjh <wngns9807@gmail.com>
2025-08-08 02:15:42 +0900

    feat: video는 실제 서버 연결

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,13 +1,11 @@
 import { setupServer } from 'msw/node';
 import { gatheringsHandlers } from './handlers/gatherings';
-import { videosHandlers } from './handlers/videos';
 import { authHandlers } from './handlers/auth';
 import { userHandlers } from './handlers/user';
 
 // MSW 서버를 설정합니다.
 export const server = setupServer(
   ...gatheringsHandlers,
-  ...videosHandlers,
   ...authHandlers,
   ...userHandlers,
 );
